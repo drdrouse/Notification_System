a2b68c5b469575a49f16caa0b6fb48520d0d2158
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-19 18:11:06 +1000

    Небольшие изменения в дизайне

--- wwwroot/js/change_account_style.js
+++ wwwroot/js/change_account_style.js
@@ -5,6 +5,13 @@ function toggleTheme() {
     // Переключаем класс dark-mode для изменения темы
     body.classList.toggle('light-mode');
 
+    const image = document.getElementById('setting');
+    if (body.classList.contains('light-mode')) {
+        image.src = '~/content/images/account/setting-lite.png?' + new Date().getTime(); // Путь к изображению для светлой темы
+    } else {
+        image.src = '~/content/images/account/setting.png?' + new Date().getTime(); // Путь к изображению для тёмной темы
+    }
+
     // Запоминаем выбранную тему в localStorage
     if (body.classList.contains('light-mode')) {
         localStorage.setItem('theme', 'light');
