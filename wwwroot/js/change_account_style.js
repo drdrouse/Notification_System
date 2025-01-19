87dc0d36dcca78f0a0b2290299d52c181b6fcf30
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-19 19:28:59 +1000

    Доработка сайдбара

--- wwwroot/js/change_account_style.js
+++ wwwroot/js/change_account_style.js
@@ -2,6 +2,8 @@
 function toggleTheme() {
     const body = document.body;
     const settingImage = document.querySelector('#settings img'); // Изображение в настройках
+    const exiteImage = document.querySelector('#exite img');
+    const accountImage = document.querySelector('#account img');
 
     // Переключаем класс light-mode для изменения темы
     body.classList.toggle('light-mode');
@@ -9,9 +11,13 @@ function toggleTheme() {
     // Меняем изображение в зависимости от выбранной темы
     if (body.classList.contains('light-mode')) {
         settingImage.src = '/content/images/account/setting-lite.png'; // Путь к изображению для светлой темы
+        exiteImage.src = '/content/images/account/exite-lite.png';
+        accountImage.src = '/content/images/account/account-lite.png';
         localStorage.setItem('theme', 'light');
     } else {
         settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
+        exiteImage.src = '/content/images/account/exite.png';
+        accountImage.src = '/content/images/account/account.png';
         localStorage.setItem('theme', 'dark');
     }
 }
@@ -20,12 +26,18 @@ function toggleTheme() {
 window.onload = () => {
     const savedTheme = localStorage.getItem('theme');
     const settingImage = document.querySelector('#settings img'); // Изображение в настройках
+    const exiteImage = document.querySelector('#exite img');
+    const accountImage = document.querySelector('#account img');
 
     if (savedTheme === 'light') {
         document.body.classList.add('light-mode');
         settingImage.src = '/content/images/account/setting-lite.png'; // Путь к изображению для светлой темы
+        exiteImage.src = '/content/images/account/exite-lite.png';
+        accountImage.src = '/content/images/account/account-lite.png';
     } else if (savedTheme === 'dark') {
         document.body.classList.remove('light-mode');
         settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
+        exiteImage.src = '/content/images/account/exite.png';
+        accountImage.src = '/content/images/account/account.png';
     }
 };
