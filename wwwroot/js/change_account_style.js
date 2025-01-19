3dcca14db2b43c48245b54ba9a3df205ea00493d
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-19 15:04:43 +1000

    Смена стилей аккаунта пользователя (смена заднего фона, смена сайдбара)

---
+++ wwwroot/js/change_account_style.js
@@ -0,0 +1,21 @@
+﻿// Функция для переключения темы
+function toggleTheme() {
+    const body = document.body;
+
+    // Переключаем класс dark-mode для изменения темы
+    body.classList.toggle('light-mode');
+
+    // Запоминаем выбранную тему в localStorage
+    if (body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+// При загрузке страницы устанавливаем сохранённую тему
+window.onload = () => {
+    if (localStorage.getItem('theme') === 'light') {
+        document.body.classList.add('light-mode');
+    }
+};
