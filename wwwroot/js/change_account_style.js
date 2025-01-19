a0a23c53d9664e8a856570d2fa270b6904a5fc54
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-19 19:17:14 +1000

    Запоминание выбранного изображения

--- wwwroot/js/change_account_style.js
+++ wwwroot/js/change_account_style.js
@@ -1,28 +1,31 @@
 ﻿// Функция для переключения темы
 function toggleTheme() {
     const body = document.body;
+    const settingImage = document.querySelector('#settings img'); // Изображение в настройках
 
-    // Переключаем класс dark-mode для изменения темы
+    // Переключаем класс light-mode для изменения темы
     body.classList.toggle('light-mode');
 
-    const settingImage = document.querySelector('#settings img');
+    // Меняем изображение в зависимости от выбранной темы
     if (body.classList.contains('light-mode')) {
         settingImage.src = '/content/images/account/setting-lite.png'; // Путь к изображению для светлой темы
-    } else {
-        settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
-    }
-
-    // Запоминаем выбранную тему в localStorage
-    if (body.classList.contains('light-mode')) {
         localStorage.setItem('theme', 'light');
     } else {
+        settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
         localStorage.setItem('theme', 'dark');
     }
 }
 
-// При загрузке страницы устанавливаем сохранённую тему
+// При загрузке страницы устанавливаем сохранённую тему и изображение
 window.onload = () => {
-    if (localStorage.getItem('theme') === 'light') {
+    const savedTheme = localStorage.getItem('theme');
+    const settingImage = document.querySelector('#settings img'); // Изображение в настройках
+
+    if (savedTheme === 'light') {
         document.body.classList.add('light-mode');
+        settingImage.src = '/content/images/account/setting-lite.png'; // Путь к изображению для светлой темы
+    } else if (savedTheme === 'dark') {
+        document.body.classList.remove('light-mode');
+        settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
     }
 };
