0f75a9f68f22e570266a32f8382b2979d5f68b59
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-20 12:41:05 +1000

    Добавление контроллера и представления для вкладки настроек

---
+++ Controllers/SettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Notification_System.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
