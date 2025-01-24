3e24fc09f662ad01c60eb7d4d3f04e9eedb0c481
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-24 11:30:23 +1000

    Подключение библиотеки классов с моделями БД

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using DataAccessLibrary.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<NotificationSystemContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
