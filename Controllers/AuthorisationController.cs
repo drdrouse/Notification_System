50718787c6e1a40e19271a56bba014016989430d
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-25 13:38:48 +1000

    Ввод логина и пароля, провекра на правильность ввода

--- Controllers/AuthorisationController.cs
+++ Controllers/AuthorisationController.cs
@@ -6,6 +6,20 @@ namespace Notification_System.Controllers
     [AllowAnonymous]
     public class AuthorisationController : Controller
     {
+        [HttpPost]
+        public IActionResult Index(string login, string password)
+        {
+            if (login == "check" && password == "123456")
+            {
+                return RedirectToAction("Index", "Account");
+            }
+
+            ViewBag.Error = "Неверно введены данные";
+            return View();
+
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             ViewData["ShowSideBarBlock"] = false; 
