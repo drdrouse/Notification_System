c0ebf6da33e6b1cd1f3c3366faab0b465cc11249
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-30 17:14:10 +1000

    Проверка на соответствие нового пароля регулярному выражению

--- Controllers/SettingController.cs
+++ Controllers/SettingController.cs
@@ -19,13 +19,34 @@ namespace Notification_System.Controllers
         public async Task<IActionResult> ChangePassword(string oldPassword, string newPassword, string confirmPassword)
         {
             string hash_oldPassword = PasswordHelper.SHA256Convert(oldPassword);
+            string hash_newPassword = PasswordHelper.SHA256Convert(newPassword);
+            string hash_confirmPassword = PasswordHelper.SHA256Convert(confirmPassword);
+            string result = "";
+
+            HttpContext.Session.Clear();
+
             if (!Change_Password.ConfirmOldPassword(hash_oldPassword, Guid.Parse(User.Identity.Name)))
             {
-                HttpContext.Session.SetString("Message", "Неверно введён текущий пароль.");
+                result += "Неверно введён текущий пароль.\n";
+                HttpContext.Session.SetString("Message", result);
                 HttpContext.Session.SetString("MessageType", "alert-error");
                 HttpContext.Session.SetString("OldPasswordValid", "input-error");
             }
+            else
+                HttpContext.Session.SetString("OldPasswordValid", "input-success");
 
+            if (!PasswordHelper.CheckPassword(newPassword))
+            {
+                result += "Пароль не соответствует требованиям:\n" +
+                    "Длина пароля более 10 символов\n" +
+                    "Должен содержать строчные и заглавные буквы латинского алфавита\n" +
+                    "Должен содержать хотя бы одну цифру от 0 до 9\n" +
+                    "Должен содержать один из специальных символов: ! ? _ - . $\n";
+                HttpContext.Session.SetString("Message", result);
+                HttpContext.Session.SetString("MessageType", "alert-error");
+                HttpContext.Session.SetString("NewPasswordValid", "input-error");
+                HttpContext.Session.SetString("ConfirmPasswordValid", "input-error");
+            }
             //if (newPassword != confirmPassword)
             //{
             //    HttpContext.Session.SetString("Message", "Новый пароль и подтверждение не совпадают.");
