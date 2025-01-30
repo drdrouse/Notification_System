569a896aba00f6bbdea968ffba26c5e03c3b887a
Dr_Drouse <dr.ykov@yandex.ru>
2025-01-30 16:26:12 +1000

    Логика проверки текущего пароля пользователя

--- Controllers/SettingController.cs
+++ Controllers/SettingController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using DataHelper;
+using DataAccessLibrary;
 
 namespace Notification_System.Controllers
 {
@@ -16,23 +18,30 @@ namespace Notification_System.Controllers
         [HttpPost]
         public async Task<IActionResult> ChangePassword(string oldPassword, string newPassword, string confirmPassword)
         {
-
-            if (newPassword != confirmPassword)
+            string hash_oldPassword = PasswordHelper.SHA256Convert(oldPassword);
+            if (!Change_Password.ConfirmOldPassword(hash_oldPassword, Guid.Parse(User.Identity.Name)))
             {
-                HttpContext.Session.SetString("Message", "Новый пароль и подтверждение не совпадают.");
+                HttpContext.Session.SetString("Message", "Неверно введён текущий пароль.");
                 HttpContext.Session.SetString("MessageType", "alert-error");
-                HttpContext.Session.SetString("OldPasswordValid", "input-success");
-                HttpContext.Session.SetString("NewPasswordValid", "input-error");
-                HttpContext.Session.SetString("ConfirmPasswordValid", "input-error");
+                HttpContext.Session.SetString("OldPasswordValid", "input-error");
             }
-            else
-            {
-                HttpContext.Session.SetString("Message", "Пароль успешно изменён!");
-                HttpContext.Session.SetString("MessageType", "alert-success");
-                HttpContext.Session.SetString("OldPasswordValid", "input-success");
-                HttpContext.Session.SetString("NewPasswordValid", "input-success");
-                HttpContext.Session.SetString("ConfirmPasswordValid", "input-success");
-            }
+
+            //if (newPassword != confirmPassword)
+            //{
+            //    HttpContext.Session.SetString("Message", "Новый пароль и подтверждение не совпадают.");
+            //    HttpContext.Session.SetString("MessageType", "alert-error");
+            //    HttpContext.Session.SetString("OldPasswordValid", "input-success");
+            //    HttpContext.Session.SetString("NewPasswordValid", "input-error");
+            //    HttpContext.Session.SetString("ConfirmPasswordValid", "input-error");
+            //}
+            //else
+            //{
+            //    HttpContext.Session.SetString("Message", "Пароль успешно изменён!");
+            //    HttpContext.Session.SetString("MessageType", "alert-success");
+            //    HttpContext.Session.SetString("OldPasswordValid", "input-success");
+            //    HttpContext.Session.SetString("NewPasswordValid", "input-success");
+            //    HttpContext.Session.SetString("ConfirmPasswordValid", "input-success");
+            //}
 
             HttpContext.Session.SetString("OpenModal", "true");
             return RedirectToAction("Index", "Setting");
