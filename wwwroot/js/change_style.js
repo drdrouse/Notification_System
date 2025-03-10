78877b63f2b8a99e4bfaf6defb75558acaed6031
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-10 20:48:41 +1000

    Решение проблемы отображения иконок

--- wwwroot/js/change_style.js
+++ wwwroot/js/change_style.js
@@ -4,9 +4,10 @@ function toggleTheme() {
     const settingImage = document.querySelector('#settings img'); // Изображение в настройках
     const exiteImage = document.querySelector('#exite img');
     const accountImage = document.querySelector('#account img');
-    const passwordImage = document.querySelector('#change_password img');
-    const themeImage = document.querySelector('#change_theme img');
-    const messengerImage = document.querySelector('#messenger img');
+   //const passwordImage = document.querySelector('#change_password img');
+    const messengerImage = document.getElementById('messengers').querySelector('img');
+    //const themeImage = document.querySelector('#change_theme img');
+
 
     // Переключаем класс light-mode для изменения темы
     body.classList.toggle('light-mode');
@@ -16,17 +17,17 @@ function toggleTheme() {
         settingImage.src = '/content/images/account/setting-lite.png'; // Путь к изображению для светлой темы
         exiteImage.src = '/content/images/account/exite-lite.png';
         accountImage.src = '/content/images/account/account-lite.png';
-        passwordImage.src = '/content/images/account/password-lite.png';
-        themeImage.src = '/content/images/account/change-theme-lite.png';
+        //passwordImage.src = '/content/images/account/password-lite.png';
+        //themeImage.src = '/content/images/account/change-theme-lite.png';
         messengerImage.src = '/content/images/account/messenger-lite.png';
         localStorage.setItem('theme', 'light');
     } else {
         settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
         exiteImage.src = '/content/images/account/exite.png';
-        accountImage.src = '/content/images/account/account.png';
-        passwordImage.src = '/content/images/account/password.png';
-        themeImage.src = '/content/images/account/change-theme.png';
+        accountImage.src = '/content/images/account/account.png';        
+        //themeImage.src = '/content/images/account/change-theme.png';
         messengerImage.src = '/content/images/account/messenger.png';
+        //passwordImage.src = '/content/images/account/password.png';
         localStorage.setItem('theme', 'dark');
     }
 }
@@ -37,25 +38,25 @@ window.onload = () => {
     const settingImage = document.querySelector('#settings img'); // Изображение в настройках
     const exiteImage = document.querySelector('#exite img');
     const accountImage = document.querySelector('#account img');
-    const passwordImage = document.querySelector('#change_password img');
-    const themeImage = document.querySelector('#change_theme img');
-    const messengerImage = document.querySelector('#messenger img');
+    //const passwordImage = document.querySelector('#change_password img');
+    //const themeImage = document.querySelector('#change_theme img');
+    const messengerImage = document.getElementById('messengers').querySelector('img');
 
     if (savedTheme === 'light') {
         document.body.classList.add('light-mode');
         settingImage.src = '/content/images/account/setting-lite.png'; // Путь к изображению для светлой темы
         exiteImage.src = '/content/images/account/exite-lite.png';
-        accountImage.src = '/content/images/account/account-lite.png';
-        passwordImage.src = '/content/images/account/password-lite.png';
-        themeImage.src = '/content/images/account/change-theme-lite.png';
+        accountImage.src = '/content/images/account/account-lite.png';        
+        //themeImage.src = '/content/images/account/change-theme-lite.png';
         messengerImage.src = '/content/images/account/messenger-lite.png';
+       // passwordImage.src = '/content/images/account/password-lite.png';
     } else if (savedTheme === 'dark') {
         document.body.classList.remove('light-mode');
         settingImage.src = '/content/images/account/setting.png'; // Путь к изображению для тёмной темы
         exiteImage.src = '/content/images/account/exite.png';
-        accountImage.src = '/content/images/account/account.png';
-        passwordImage.src = '/content/images/account/password.png';
-        themeImage.src = '/content/images/account/change-theme.png';
+        accountImage.src = '/content/images/account/account.png';        
+        //themeImage.src = '/content/images/account/change-theme.png';
         messengerImage.src = '/content/images/account/messenger.png';
+        //passwordImage.src = '/content/images/account/password.png';
     }
 };
