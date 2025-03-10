b38f44b9f2da3c553c2022574776d90eeb5737c7
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-10 16:55:15 +1000

    Скрипт открывает и закрывает область с данными о месенджере

--- wwwroot/js/messenger.js
+++ wwwroot/js/messenger.js
@@ -1,3 +1,22 @@
-﻿document.getElementById('toggleButton').addEventListener('click', function () {
-    console.log('Кнопка нажата!');
+﻿// Получим все кнопки
+const buttons = document.querySelectorAll('button[data-target]');
+
+// Проходимся по каждой кнопке и добавляем обработчики событий
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Получаем ID целевой области
+        const targetId = button.dataset.target;
+
+        // Находим целевую область
+        const targetArea = document.getElementById(targetId);
+
+        // Меняем класс области 
+        if (targetArea.classList.contains('messanger-data-hidden')) {
+            targetArea.classList.remove('messanger-data-hidden');
+            targetArea.classList.add('messanger-data-visible');
+        } else {
+            targetArea.classList.remove('messanger-data-visible');
+            targetArea.classList.add('messanger-data-hidden');
+        }
+    });
 });
