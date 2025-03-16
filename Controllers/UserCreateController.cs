65c03545f2530cb8dec596300802bdd4a87525f8
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-16 20:50:12 +1000

    Настройка стилей для светлой темы

--- Controllers/UserCreateController.cs
+++ Controllers/UserCreateController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Notification_System.Controllers
 {
+
+
     public class UserCreateController : Controller
     {
+
+        private readonly NotificationSystemContext _notificationSystemContext;
+
+        public UserCreateController(NotificationSystemContext notificationSystemContext)
+        {
+            _notificationSystemContext = notificationSystemContext;
+        }
+
+
         [Authorize(Roles = "UserCreator,Admin")]
         public IActionResult Index()
         {
             ViewData["ShowSideBarBlock"] = true;
-            return View();
+            var profiles = _notificationSystemContext.Profiles.ToList();
+            return View(profiles);
         }
     }
 }
