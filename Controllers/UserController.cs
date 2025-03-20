c99cdd96361f4eced213afc921d050c0faeb003d
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-20 17:35:07 +1000

    Создание контроллера и представления для user. Для роли UserCreater

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notification_System.Controllers
+{
+    public class UserController : Controller
+    {
+        [Authorize(Roles = "UserCreator,Admin")]
+        public IActionResult Index()
+        {
+            ViewData["ShowSideBarBlock"] = true;
+            return View();
+        }
+    }
+}
