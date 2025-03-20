111db05ab6ac22961b3d8a41b867784c049952ea
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-20 17:58:38 +1000

    Передача индентификатора в контроллер

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Notification_System.Controllers
 {
     public class UserController : Controller
     {
+
+        private readonly NotificationSystemContext _notificationSystemContext;
+
+        public UserController(NotificationSystemContext notificationSystemContext)
+        {
+            _notificationSystemContext = notificationSystemContext;
+        }
+
         [Authorize(Roles = "UserCreator,Admin")]
-        public IActionResult Index()
+
+        public IActionResult Index(int tabnum)
         {
             ViewData["ShowSideBarBlock"] = true;
+            //var profile = _notificationSystemContext.Profiles.Where(p => p.ProfileTabNum == tabnum).FirstOrDefault();
+            //var account = _notificationSystemContext.Accounts.Where(a => a.ProfileId == profile.ProfileId).FirstOrDefault();
             return View();
         }
     }
