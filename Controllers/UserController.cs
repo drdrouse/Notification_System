4a25d8071ebcad3d3a9402d1992572171c07d2ce
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-20 19:52:48 +1000

    Основная часть карточки пользователя

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Notification_System.Controllers
 {
@@ -19,9 +20,11 @@ namespace Notification_System.Controllers
         public IActionResult Index(int tabnum)
         {
             ViewData["ShowSideBarBlock"] = true;
-            //var profile = _notificationSystemContext.Profiles.Where(p => p.ProfileTabNum == tabnum).FirstOrDefault();
-            //var account = _notificationSystemContext.Accounts.Where(a => a.ProfileId == profile.ProfileId).FirstOrDefault();
-            return View();
+            var profile = _notificationSystemContext.Profiles.Where(p => p.ProfileTabNum == tabnum).FirstOrDefault();
+            var account = _notificationSystemContext.Accounts.Where(a => a.ProfileId == profile.ProfileId).
+                Include(a => a.Profile).ThenInclude(p => p.Mail).ThenInclude(m => m.TypeMail).
+                Include(a => a.Profile).ThenInclude(p => p.Phones).ThenInclude(ph => ph.TypePhone).ToList();
+            return View(account);
         }
     }
 }
