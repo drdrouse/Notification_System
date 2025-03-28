0a2d041d53233755c03baf4fceedb6dae640d063
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-28 10:30:22 +1000

    Корректировка выбора значений

---
+++ wwwroot/js/zeroing_checkbox.js
@@ -0,0 +1,23 @@
+﻿const allCheckboxes = document.querySelectorAll('.checkbox-group input[type="checkbox"]');
+
+const specialCheckbox = document.querySelector('#Admin');
+
+// Обработчик для всех чекбоксов
+allCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+        // Если меняется состояние специального чекбокса
+        if (this === specialCheckbox) {
+            // Обнуляем все остальные
+            allCheckboxes.forEach(cb => {
+                if (cb !== specialCheckbox) {
+                    cb.checked = false;
+                }
+            });
+        } else {
+            // Если меняется обычный чекбокс
+            if (specialCheckbox.checked) {
+                specialCheckbox.checked = false;
+            }
+        }
+    });
+});
