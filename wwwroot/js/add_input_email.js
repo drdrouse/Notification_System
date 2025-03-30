936d32ff965ef12daaac89205b48f13eaa3638ac
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-30 15:26:18 +1000

    Области для ввода телефонов и почты пользователя

---
+++ wwwroot/js/add_input_email.js
@@ -0,0 +1,29 @@
+﻿function createNewFieldEmail() {
+    // Создаем контейнер для одного набора полей
+    const fieldContainer = document.createElement('div');
+    fieldContainer.classList.add('field-container');
+
+    // Создаем метку
+    const label = document.createElement('label');
+    label.htmlFor = 'srvice_setting' + fieldsContainer.childNodes.length; // Уникальное имя для каждого поля
+    label.textContent = 'Почта';
+    fieldContainer.appendChild(label);
+
+    // Создаем поле ввода
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'srvice_setting' + fieldsContainer.childNodes.length; // Уникальное ID для каждого поля
+    fieldContainer.appendChild(input);
+
+    return fieldContainer;
+}
+
+// Обработка нажатия на кнопку
+const addFieldButton = document.getElementById('addEmail');
+const fieldsContainer = document.getElementById('emailsContainer');
+
+addFieldButton.addEventListener('click', () => {
+    // Создаем новое поле и добавляем его в контейнер
+    const newField = createNewFieldEmail();
+    fieldsContainer.appendChild(newField);
+});
---
+++ wwwroot/js/add_input_phone.js
@@ -0,0 +1,30 @@
+﻿function createNewFieldPhone() {
+    // Создаем контейнер для одного набора полей
+    const fieldContainer = document.createElement('div');
+    fieldContainer.classList.add('field-container');
+
+    // Создаем метку
+    const label = document.createElement('label');
+    label.htmlFor = 'srvice_setting' + fieldsContainer.childNodes.length; // Уникальное имя для каждого поля
+    label.textContent = 'Телефон';
+    fieldContainer.appendChild(label);
+
+    // Создаем поле ввода
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.id = 'srvice_setting' + fieldsContainer.childNodes.length; // Уникальное ID для каждого поля
+    fieldContainer.appendChild(input);
+
+    return fieldContainer;
+}
+
+// Обработка нажатия на кнопку
+// Убедитесь, что эти переменные объявляются только один раз
+const addFieldButton = document.getElementById('addPhone');
+const fieldsContainer = document.getElementById('phoneContainer');
+
+addFieldButton.addEventListener('click', () => {
+    // Создаем новое поле и добавляем его в контейнер
+    const newField = createNewFieldPhone();
+    fieldsContainer.appendChild(newField);
+});
