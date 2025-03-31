aff3a94c14f27514b956a5997595bdda4bd865ab
Dr_Drouse <dr.ykov@yandex.ru>
2025-03-31 22:15:51 +1000

    Добавление проверки введённых данных

--- Controllers/UserCreateController.cs
+++ Controllers/UserCreateController.cs
@@ -35,5 +35,28 @@ namespace Notification_System.Controllers
             HttpContext.Session.Clear(); // Очищаем всю сессию
             return RedirectToAction("Index", "UserCreate"); // Редирект на главную страницу сервисов
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(string Surname)
+        {
+
+            string result_error = "";
+            string result_success = "Пользователь успешно создан";
+
+            HttpContext.Session.Clear();
+
+            if (Surname == "Иванов")
+            {
+                result_error += "Неверно введёна фамилия\n";
+                HttpContext.Session.SetString("Message", result_error);
+                HttpContext.Session.SetString("MessageType", "alert-error");
+                HttpContext.Session.SetString("SurnameValid", "input-error");
+            }
+            else
+                HttpContext.Session.SetString("SurnameValid", "input-success");
+
+            HttpContext.Session.SetString("OpenModal", "true");
+            return RedirectToAction("Index", "UserCreate");
+        }
     }
 }
